82ebf79a0e71b75bd164de581e00e9b340b82121
Crowdbotics <engineering@crowdbotics.com>
2022-02-23 19:18:31 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Testing(models.Model):
+    "Generated Model"
+    some = models.BigIntegerField()
+    things = models.CharField(
+        max_length=256,
+    )
